37410ff85d837180ff9b628ead4edcaa85ad07d8
Pankaj Bisht <130260590+pankajbisht1909@users.noreply.github.com>
2024-05-27 22:06:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Calculator from './calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MAKE MATHS EASY</h1>
+        <Calculator />
+      </header>
+    </div>
+  );
+}
+export default App;
